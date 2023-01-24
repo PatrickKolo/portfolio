71a7b222b7b35dcc98a1cb03453ce5e53977d55a
Patrick Kolodziej <116544154+PatrickKolo@users.noreply.github.com>
2023-01-24 14:09:38 +0100

    myskills finished

--- src/app/myskills/myskills.component.ts
+++ src/app/myskills/myskills.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyskillsComponent implements OnInit {
 
+  public skills: string[] = ['angular.png','typescript.png', 'javascript.png', 'html.png', 'firebase.png', 'git.png', 'css.png', 'rest-api.png', 'scrum.png', 'materialdesign.png']
+  public skillName: string[] = ['Angular','Typescript', 'Javascript', 'HTML', 'Firebase', 'Git', 'CSS', 'Rest-API', 'Scrum', 'Material Design']
+
   constructor() { }
 
   ngOnInit(): void {
