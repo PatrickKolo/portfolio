03ae620ab2c4dd844bc14d4eb6754f167ae15d30
Patrick Kolodziej <116544154+PatrickKolo@users.noreply.github.com>
2023-01-24 15:02:54 +0100

    portfolio first draft

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StartpageComponent } from './startpage/startpage.component';
+
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ImprintComponent } from './imprint/imprint.component';
--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PortfolioComponent implements OnInit {
 
+  public projectnames: string[] = ['Join', 'El Pollo Loco', 'Rong of fire', 'Pokedex'];
+  public projectimages: string[] = ['join-placeholder .png', 'sharkie-placeholder.png', 'pokedex-placeholder.png', 'simple-CRM-placeholder.png'];
+  public methods: string[] = ['HTML | CSS | JavaScript', 'HTML | CSS | JavaScript', 'Angular | Firebase', 'HTML | CSS | JavaScript | API'];
+  public descriptions: string[] = ['Task manager inspired by the Kanban System. Create and organize tasks using drag and drop function, assign users and categories',
+'A simple Jump-and-Run game based on an object-oriented approach. Help Pepe to collect coins and salsa bottles to defeat a giant chicken', 
+'[Under construction]A very simple Customer Relationship Management system working with CRUD functionality[Under construction]', 'Based on the PokéAPI, a simple library that provides and catalogues pokemon information',];
+
   constructor() { }
 
   ngOnInit(): void {
