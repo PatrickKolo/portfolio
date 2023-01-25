0f2d66b4756838bf70e05ead58ed7aa6a3cb9838
Patrick Kolodziej <116544154+PatrickKolo@users.noreply.github.com>
2023-01-25 18:58:11 +0100

    links added

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -13,9 +13,9 @@ export class PortfolioComponent implements OnInit {
   public descriptions: string[] = ['Task manager inspired by the Kanban System. Create and organize tasks using drag and drop function, assign users and categories',
     'A simple Jump-and-Run game based on an object-oriented approach. Help Pepe to collect coins and salsa bottles to defeat a giant chicken',
     '[Under construction]A very simple Customer Relationship Management system working with CRUD functionality[Under construction]', 'Based on the PokéAPI, a simple library that provides and catalogues pokemon information',];
-  public livetests: string[] = ['https://patrick-kolodziej.developerakademie.net/JOIN-Kanban/index.html', 'https://patrick-kolodziej.developerakademie.net/El_Pollo_Loco/index.html',
+  public livetests: string[] = ['https://patrick-kolodziej.developerakademie.net/join/src/index.html', 'http://patrick-kolodziej.developerakademie.net/El%20Pollo%20Loco/index.html',
     '#', 'https://patrick-kolodziej.developerakademie.net/Pokedex/index.html'];
-  public github: string[] = ['https://github.com/PatrickKolo/JOIN-Kanban', 'https://github.com/PatrickKolo/El-Pollo-Loco', '#', 'https://github.com/PatrickKolo/ringoffire',];
+  public github: string[] = ['https://github.com/PatrickKolo/Join', 'https://github.com/PatrickKolo/El-Pollo-Loco', '#', 'https://github.com/PatrickKolo/ringoffire',];
 
 
   constructor() { }
