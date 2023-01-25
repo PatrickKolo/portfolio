71c855695875a5690194d0865fbbefba49f1ee41
Patrick Kolodziej <116544154+PatrickKolo@users.noreply.github.com>
2023-01-25 18:42:53 +0100

    almost finished

--- src/app/myskills/myskills.component.ts
+++ src/app/myskills/myskills.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-myskills',
@@ -10,9 +12,37 @@ export class MyskillsComponent implements OnInit {
   public skills: string[] = ['angular.png','typescript.png', 'javascript.png', 'html.png', 'firebase.png', 'git.png', 'css.png', 'rest-api.png', 'scrum.png', 'materialdesign.png']
   public skillName: string[] = ['Angular','Typescript', 'Javascript', 'HTML', 'Firebase', 'Git', 'CSS', 'Rest-API', 'Scrum', 'Material Design']
 
-  constructor() { }
+  constructor(public router:Router, private headerComponent:HeaderComponent) { }
 
   ngOnInit(): void {
   }
+
+  public scroll(id: string) {
+    if (this.router.url == '/imprint') {
+      this.returnToStartpage();
+      this.setLinksToFalse();
+    } else {
+      this.scrollToArea(id);
+    }
+  }
+  
+  setLinksToFalse() {
+    this.headerComponent.aboutHeadClicked = false;
+    this.headerComponent.skillClicked = false;
+    this.headerComponent.portfolioClicked = false;
+  }
+
+  returnToStartpage() {
+    window.location.href = 'http://localhost:4200/'
+  }
+  
+  scrollToArea(id:string) {
+    const area = document.getElementById(id);
+    area?.scrollIntoView({
+      behavior: 'smooth',
+      block: "start",
+      });
+  }
+
 
 }
--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -8,15 +8,23 @@ import { Component, OnInit } from '@angular/core';
 export class PortfolioComponent implements OnInit {
 
   public projectnames: string[] = ['Join', 'El Pollo Loco', 'Simple CRM', 'Pokedex'];
-  public projectimages: string[] = ['join.png', 'loco-placeholder.png', 'simple-crm.png','pokedex.png'];
+  public projectimages: string[] = ['join.png', 'loco-placeholder.png', 'simple-crm.png', 'pokedex.png'];
   public methods: string[] = ['HTML | CSS | JavaScript', 'HTML | CSS | JavaScript', 'Angular | Firebase', 'HTML | CSS | JavaScript | API'];
   public descriptions: string[] = ['Task manager inspired by the Kanban System. Create and organize tasks using drag and drop function, assign users and categories',
-'A simple Jump-and-Run game based on an object-oriented approach. Help Pepe to collect coins and salsa bottles to defeat a giant chicken', 
-'[Under construction]A very simple Customer Relationship Management system working with CRUD functionality[Under construction]', 'Based on the PokéAPI, a simple library that provides and catalogues pokemon information',];
+    'A simple Jump-and-Run game based on an object-oriented approach. Help Pepe to collect coins and salsa bottles to defeat a giant chicken',
+    '[Under construction]A very simple Customer Relationship Management system working with CRUD functionality[Under construction]', 'Based on the PokéAPI, a simple library that provides and catalogues pokemon information',];
+  public livetests: string[] = ['https://patrick-kolodziej.developerakademie.net/JOIN-Kanban/index.html', 'https://patrick-kolodziej.developerakademie.net/El_Pollo_Loco/index.html',
+    '#', 'https://patrick-kolodziej.developerakademie.net/Pokedex/index.html'];
+  public github: string[] = ['https://github.com/PatrickKolo/JOIN-Kanban', 'https://github.com/PatrickKolo/El-Pollo-Loco', '#', 'https://github.com/PatrickKolo/ringoffire',];
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  visitProject(url:string) {
+    window.open(url, '_blank');
   }
 
 }
